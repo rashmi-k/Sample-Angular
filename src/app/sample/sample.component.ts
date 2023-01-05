67e30f6fab5cf064ae75e0c06ebf5527e1e0e093
rashmi-k <krashmi887@gmail.com>
2023-01-05 14:45:22 +0530

    2 files modified, 3 added

---
+++ src/app/sample/sample.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'sample-main',
+  templateUrl: './sample/sample.component.html',
+  styleUrls: ['./sample/sample.component.css'],
+})
+export class SampleComponent {
+  name = 'In sample Works';
+}
